73b543a319d1d51cb2edc00ae1ad6ab5eb9d682e
Charcrede <charcrede@gmail.com>
2026-02-22 02:28:26 +0100

    github callback url

--- src/auth/strategies/github.strategy.ts
+++ src/auth/strategies/github.strategy.ts
@@ -11,7 +11,7 @@ export class GithubStrategy extends PassportStrategy(Strategy, 'github') {
     super({
       clientID: process.env.GITHUB_CLIENT_ID!,
       clientSecret: process.env.GITHUB_CLIENT_SECRET!,
-      callbackURL: process.env.BACK_URL! + 'http://localhost:3000/auth/github/callback',
+      callbackURL: process.env.BACK_URL! + '/auth/github/callback',
       scope: ['user:email'],
     });
   }
